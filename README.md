7c7e50f0a13ff10b3cd8bd35b02f0245289ed364
gskorupa <g.skorupa@gmail.com>
2026-04-02 09:05:04 +0200

    Update README and disable reservation endpoints
    Rewrite README to describe Signomix Collector
    
    Replace reservation handling in ForReservationsAdapter with throwing
    unauthorized ServiceException and add TODOs for
    authentication/authorization.
    Comment out previous persistence/event logic and reformat method
    parameters and imports.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Projekt LoRaWAN w Dworze Kombornia (LDD)
+# Signomix Collector
 
-Projekt zawiera kody źródłowe mikroserwisu `ldd-reservations`, który będzie zintegrowany z platformą Signomix i implementuje funkcjonalności dedykoane LDD (m.inn. integrację z systemem rezerwacji).
-Dodatkowo: zarządzanie zadaniami i problemami oraz komunikacja.
+Signomix Collector is a software component that collects data from various sources and sends it to the Signomix platform for processing and analysis. It is designed to be flexible and can be configured to collect data from a wide range of sources
+
+Collector subscribes to signomix-broker MQTT topics and runs methods collecting data in ressponse to received messages. Several methods are implemented:
+- receiving hotel reservation data
+- store Signomix twin object representing a hotel room (twin is a digital representation of a physical object, in this case, a hotel room).
--- src/main/java/com/signomix/collector/adapters/driving/ForReservationsAdapter.java
+++ src/main/java/com/signomix/collector/adapters/driving/ForReservationsAdapter.java
@@ -1,15 +1,10 @@
 package com.signomix.collector.adapters.driving;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.openapi.annotations.media.PatternProperty;
-import org.jboss.logging.Logger;
-
 import com.signomix.collector.app.exception.ServiceException;
 import com.signomix.collector.app.logic.inteahotel.InteaReservationLogic;
 import com.signomix.collector.app.logic.inteahotel.dto.ReservationDto;
 import com.signomix.collector.app.ports.driving.AuthPort;
 import com.signomix.common.User;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
@@ -20,6 +15,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.openapi.annotations.media.PatternProperty;
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 @Path("/api/reservations")
@@ -47,47 +45,57 @@ public class ForReservationsAdapter {
     @Path("/status/{roomPrefix}/{id}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String status(@HeaderParam("Authentication") String token,
-            @PathParam(value = "roomPrefix") String roomPrefix,
-            @PathParam("id") Integer id) {
+    public String status(
+        @HeaderParam("Authentication") String token,
+        @PathParam(value = "roomPrefix") String roomPrefix,
+        @PathParam("id") Integer id
+    ) {
         User user = authPort.getUser(token);
         if (null == user) {
             throw new ServiceException(unauthorizedException);
         }
-        Integer status = reservationLogic.getRoomReservationStatus(id, roomPrefix);
-        return "" + status;
+        //TODO: implement authentication and authorization
+        //Integer status = reservationLogic.getRoomReservationStatus(id, roomPrefix);
+        //return "" + status;
+        throw new ServiceException(unauthorizedException);
     }
 
     /**
      * Zmiana statusu rezerwacji dla pokoju
-     * 
+     *
      * @param reservation
      * @return
-     * 
+     *
      *         Example CURL request:
      *         curl -i -H "Content-Type: application/json" -d
      *         '{"roomId":2,"status":5}'
      *         "http://localhost:9999/api/reservations/status"
      */
-    
+
     @POST
     @Path("/{pathParam}/status")
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
-    public String setStatus(@HeaderParam("Authentication") String token, @PathParam("pathParam") String roomPrefix, ReservationDto reservation) {
+    public String setStatus(
+        @HeaderParam("Authentication") String token,
+        @PathParam("pathParam") String roomPrefix,
+        ReservationDto reservation
+    ) {
         User user = authPort.getUser(token);
         if (null == user) {
             throw new ServiceException(unauthorizedException);
         }
-        try {
-            long timestamp = System.currentTimeMillis();
-            reservationLogic.saveRoomReservationStatus(roomPrefix, reservation.roomId, reservation.status, timestamp);
-            reservationLogic.createRoomDataEvent(roomPrefix, reservation.roomId, reservation.status, reservation.arrivalDate, timestamp);
-            return "OK";
-        } catch (Exception e) {
-            logger.error("Error setting status for room " + reservation.roomId + " to " + reservation.status);
-            e.printStackTrace();
-            return "ERROR";
-        }
+        //TODO: implement authentication and authorization
+        // try {
+        //     long timestamp = System.currentTimeMillis();
+        //     reservationLogic.saveRoomReservationStatus(roomPrefix, reservation.roomId, reservation.status, timestamp);
+        //     reservationLogic.createRoomDataEvent(roomPrefix, reservation.roomId, reservation.status, reservation.arrivalDate, timestamp);
+        //     return "OK";
+        // } catch (Exception e) {
+        //     logger.error("Error setting status for room " + reservation.roomId + " to " + reservation.status);
+        //     e.printStackTrace();
+        //     return "ERROR";
+        // }
+        throw new ServiceException(unauthorizedException);
     }
 }
